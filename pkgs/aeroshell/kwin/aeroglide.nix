51e624bc610b5d2b8e82c73f87c8ec7e7cd077e9
Nia <hakase@litdevs.org>
2026-08-19 03:33:24 +0200

    aeroglide: fix aeroshell on X11

--- pkgs/aeroshell/kwin/aeroglide.nix
+++ pkgs/aeroshell/kwin/aeroglide.nix
@@ -16,6 +16,11 @@ stdenv.mkDerivation {
     cd effects_cpp/${session}/aeroglide
     substituteInPlace src/metadata.json src/CMakeLists.txt --replace-fail \
       "kwin_aeroglide_config" "kwin_aeroglide_${session}_config"
+  '' + lib.optionalString (session == "x11") ''
+    # on X11 whether the glide plays for plasmashell windows is reliant on this hardcoded
+    # window class, since patched plasmashell is built as aeroshell they were gliding :(
+    substituteInPlace src/glide.cpp --replace-fail \
+      '"plasmashell org.kde.plasmashell"' '"aeroshell plasmashell"'
   '';
   buildInputs = [ kdePackages.qttools ] 
     ++ lib.optionals (session == "x11") [ kdePackages.kwin-x11 ]
